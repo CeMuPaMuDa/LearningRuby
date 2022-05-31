bde8c67da7a1558589c2dce1a3a00a0a9e864756
CeMuPaMuDa <bepyc9i@gmail.com>
2022-05-31 21:28:06 +0300

    task 8, version 2

--- GeekBrains/I_RubyPart1/l4.8_distance_v1.rb
+++ GeekBrains/I_RubyPart1/l4.8_distance_v1.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+frozen_string_literal: true
 
 a = { x: 3, y: 7 }
 b = { x: -1, y: 5 }
@@ -6,3 +6,4 @@ b = { x: -1, y: 5 }
 result = Math.sqrt((b[:x] - a[:x])**2 + (b[:y] - a[:y])**2)
 
 puts "Paccтояние между точками: #{result.round(2)}"
+
---
+++ GeekBrains/I_RubyPart1/l4.8_distance_v2.rb
@@ -0,0 +1,7 @@
+require 'ostruct'
+
+a = OpenStruct.new(x: 3, y: 7)
+b = OpenStruct.new(x: -1, y: 5)
+
+result = Math.sqrt((b.x - a.x)**2 + (b.y - a.y)**2)
+puts "Paccтояние между точками: #{result.round(2)}"
