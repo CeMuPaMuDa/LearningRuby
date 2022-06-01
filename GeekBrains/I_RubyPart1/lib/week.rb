d8ffaef4bbd4382b33c362eb4dd8cbc2e4c71c9b
CeMuPaMuDa <bepyc9i@gmail.com>
2022-06-01 16:09:42 +0300

    task 1

--- GeekBrains/I_RubyPart1/lib/week.rb
+++ GeekBrains/I_RubyPart1/lib/week.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 
+MONDAY = 'понедельник'
+TUESDAY = 'вторник'
+WEDNESDAY = 'среда'
+THURSDAY = 'четверг'
+FRIDAY = 'пятница'
+SATURDAY = 'суббота'
+SUNDAY = 'воскресение'
+
 WEEK_ARRAY = %w[понедельник вторник среда четверг пятница суббота воскресение].freeze
+WEEK_CONS = [MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY].freeze
