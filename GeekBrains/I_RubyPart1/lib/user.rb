4eee1ad92a04ba41b20471c2a14d50ced559a0be
CeMuPaMuDa <bepyc9i@gmail.com>
2022-06-01 13:03:17 +0300

    task 5
    rubocop -A

--- GeekBrains/I_RubyPart1/lib/user.rb
+++ GeekBrains/I_RubyPart1/lib/user.rb
@@ -3,9 +3,16 @@
 class User
   attr_reader :name, :surname, :patronymic
 
+  @@counter = 0
+
   def initialize(name:, surname:, patronymic:)
     @name = name
     @surname = surname
     @patronymic = patronymic
+    @@counter += 1
+  end
+
+  def self.counter
+    @@counter
   end
 end
