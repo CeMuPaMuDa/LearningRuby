36709536a38ec294a17df31e341fec50a2de1df2
CeMuPaMuDa <bepyc9i@gmail.com>
2022-06-16 15:25:29 +0300

    task 7

--- GeekBrains/Ruby/lib/moduls/calculating.rb
+++ GeekBrains/Ruby/lib/moduls/calculating.rb
@@ -4,4 +4,17 @@ module Calculating
   def my_sum(*numbers)
     numbers.reduce(:+)
   end
+
+  def r_fibonacci(num)
+    return 0 if num == 1
+    return 1 if [2, 3].include?(num)
+
+    r_fibonacci(num - 1) + r_fibonacci(num - 2)
+  end
+
+  def fibonacci(num)
+    f = ((1 + Math.sqrt(5)) / 2)**(num - 1)
+    s = ((1 - Math.sqrt(5)) / 2)**(num - 1)
+    ((f - s) / Math.sqrt(5)).round
+  end
 end
