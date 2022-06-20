8c969157191a400585df3c29fbb72e419649c4c8
CeMuPaMuDa <bepyc9i@gmail.com>
2022-06-20 23:19:25 +0300

    task 3

---
+++ ITVDN/Ruby/app/RE_lesson1/l1.3_user.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+print 'Enter your name: '
+first_name = gets.chomp
+print 'Enter your position: '
+position = gets.chomp
+
+puts "Greetings, #{position} #{first_name}!"
